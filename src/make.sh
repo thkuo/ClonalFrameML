9f0c3645c1b29a1d664d9d7a6bb76b80ee3e72d3
Tzu-Hao Kuo <tku16@helmholtz-hzi.de>
2017-12-08 15:53:19 +0100

    Update make.sh

--- src/make.sh
+++ src/make.sh
@@ -1,3 +1,3 @@
 echo "#define ClonalFrameML_GITRevision \"`git describe --tags`\"" > version.h
-g++ main.cpp -o ClonalFrameML -I ./ -I ./myutils -I ./coalesce -O3
+g++ main.cpp -o clonalframeml -I ./ -I ./myutils -I ./coalesce -O3
 
